51eaec39244b6282f90f1e67abd7b081dcad2ad3
Ahmed Charles <acharles@outlook.com>
2016-07-21 10:41:53 +0000

    Add Lua 5.3.3 (plus bug fixes) to the build.

---
+++ build.rs
@@ -0,0 +1,32 @@
+// Copyright (C) 2016 Ahmed Charles - acharles@outlook.com
+// Distributed under the MIT License.
+//    (See accompanying file LICENSE.txt or copy at
+//          http://opensource.org/licenses/MIT)
+
+use std::process::Command;
+
+#[cfg(target_os = "macos")]
+fn os(cmd: &mut Command) -> &mut Command { cmd.arg("macos") }
+
+#[cfg(target_os = "linux")]
+fn os(cmd: &mut Command) -> &mut Command { cmd.arg("linux").arg("MYCFLAGS=-fPIC") }
+
+#[cfg(not(any(target_os = "macos", target_os = "linux")))]
+fn os(cmd: &mut Command) -> &mut Command { cmd.arg("generic") }
+
+fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+
+    assert!(os(Command::new("make").arg("-C").arg("puc-lua")).status().unwrap().success());
+    assert!(Command::new("cp").arg("puc-lua/src/liblua.a").arg(&out_dir).status().unwrap().success());
+    assert!(Command::new("cp").arg("puc-lua/src/lua").arg(&out_dir).status().unwrap().success());
+    assert!(Command::new("cp").arg("puc-lua/src/luac").arg(&out_dir).status().unwrap().success());
+    assert!(Command::new("make").arg("-C").arg("puc-lua").arg("clean").status().unwrap().success());
+
+    println!("cargo:rustc-link-lib=static=lua");
+    println!("cargo:rustc-link-search=native={}", out_dir);
+
+    for entry in std::fs::read_dir("puc-lua/src").unwrap() {
+        println!("cargo:rerun-if-changed={}", entry.unwrap().path().display());
+    }
+}
